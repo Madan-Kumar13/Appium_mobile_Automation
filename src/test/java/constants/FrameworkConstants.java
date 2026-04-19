2f6fd50ac4633b41639b5e41cb881c843ba04a5b
Madan-Kumar13 <madankumarsp969@gmail.com>
2026-04-19 13:45:41 +0530

    DOB Completed stuck in OTP Page

---
+++ src/test/java/constants/FrameworkConstants.java
@@ -0,0 +1,9 @@
+package constants;
+
+public class FrameworkConstants {
+
+    public static final String BASE_URL = "https://your-api-url.com";
+    public static final String OTP_ENDPOINT = "/get-otp";
+
+    public static final int EXPLICIT_WAIT = 10;
+}
---
+++ src/test/java/utils/WaitUtils.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class WaitUtils {
+
+    public static void waitForSeconds(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
